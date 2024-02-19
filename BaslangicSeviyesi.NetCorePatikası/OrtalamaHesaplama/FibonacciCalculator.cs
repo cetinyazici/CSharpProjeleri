2655a8dc0d41fe69ae2ffb5388aff43624a748ca
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-19 19:24:56 +0300

    added FibonacciCalculator

---
+++ BaslangicSeviyesi.NetCorePatikası/OrtalamaHesaplama/FibonacciCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrtalamaHesaplama
+{
+    public class FibonacciCalculator
+    {
+        public double GetFibonacciAverage(int depth)
+        {
+            if (depth <= 0)
+            {
+                return 0;
+            }
+
+            long[] fibonacciSeries = GenerateFibonacciSeries(depth);
+            double sum = 0;
+
+            foreach (long num in fibonacciSeries)
+            {
+                sum += num;
+            }
+
+            return sum / depth;
+        }
+
+        private long[] GenerateFibonacciSeries(int depth)
+        {
+            long[] series = new long[depth];
+            series[0] = 0;
+            if (depth == 1)
+            {
+                return series;
+            }
+            series[1] = 1;
+
+            for (int i = 2; i < depth; i++)
+            {
+                series[i] = series[i - 1] + series[i - 2];
+            }
+
+            return series;
+        }
+    }
+}
+
--- BaslangicSeviyesi.NetCorePatikası/OrtalamaHesaplama/Program.cs
+++ BaslangicSeviyesi.NetCorePatikası/OrtalamaHesaplama/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using OrtalamaHesaplama;
+
+internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Fibonacci serisinin derinliğini giriniz:");
+        int depth = Convert.ToInt32(Console.ReadLine());
+
+        FibonacciCalculator calculator = new FibonacciCalculator();
+        double average = calculator.GetFibonacciAverage(depth);
+
+        Console.WriteLine("Ortalama: " + average);
     }
 }

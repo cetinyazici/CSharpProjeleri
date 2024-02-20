bebf42206614ae74b1857ca49e41da62befe8b90
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-20 18:33:14 +0300

    added UcgenCizme

---
+++ BaslangicSeviyesi.NetCorePatikası/UcgenCizme/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Üçgenin Boyutunu giriniz: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= size; i++)
+        {
+
+            for (int j = 1; j <= size - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

ecf2105c6d3e72aee891e78947efd5eed7af53c6
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-23 20:31:16 +0300

    added KarakterTerstenYazdirma

---
+++ BaslangicSeviyesi.NetCorePatikası/KarakterTerstenYazdirma/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input: ");
+        string input = Console.ReadLine();
+
+        string output = MoveFirstCharacterToEnd(input);
+
+        Console.WriteLine("Output: " + output);
+    }
+
+    static string MoveFirstCharacterToEnd(string input)
+    {
+        char firstChar = input[0];
+        string result = input.Substring(1);
+        result += firstChar;
+        return result;
+    }
+}
